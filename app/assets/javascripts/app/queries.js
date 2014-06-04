972ffe98846e635bab7b137854c91257c5065cbf
Jay Deuskar <coffeejay@me.com>
2014-06-04 11:19:48 -0400

    Add rotating text

--- app/assets/javascripts/app/queries.js
+++ app/assets/javascripts/app/queries.js
@@ -40,6 +40,17 @@ function populate_table(table_index, url)
 
 $(document).ready(function()
 {
+
+	var c=0, words=['PE Ratio','total debt/equity ratio','revenue','quarterly earnings growth'];
+
+
+	function loop(){
+	  $('#changer').delay(3000).fadeTo(300,0,function(){
+	     $(this).text( words[++c%words.length]).fadeTo(300,1,loop);
+	  });
+	}
+
+	loop();
 	$("form").on('click', "#query-vs", function()
 	{
 		new_fields = '<div class="row"><div class="col-lg-3"></div><div class="col-lg-2"><div class="form-group"><input class="form-control input-lg" id="ticker" name="ticker" placeholder="TCKR" type="text"></div></div><div class="col-lg-4"><div class="form-group"></div></div><div class="col-lg-3"></div></div>';
