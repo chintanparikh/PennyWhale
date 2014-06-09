847937be09efc632d049abf1e10a8165be144a2f
Chintan Parikh <chintan@pennywhale.com>
2014-06-09 13:19:31 -0400

    Revert "Revert "Add errors""
    This reverts commit dc7910da966e7b8fe2d4461d227d7d03379c1acb.

--- app/controllers/app/queries_controller.rb
+++ app/controllers/app/queries_controller.rb
@@ -7,13 +7,23 @@ class App::QueriesController < App::BaseController
 	def execute
 		stocks = params[:query].scan(/\$([a-zA-Z.]+)/).flatten.map{|stock| Stock.find(stock)}
 
+		if stocks.empty?
+			flash.now[:danger] = "No stock tickers entered." 
+			return false
+		end
+
 		query = params[:query].gsub("'", '').gsub('"', '')
 
 		like = (Rails.env.production? or Rails.env.staging?) ? "ILIKE" : "LIKE"
 
 		phrases = Phrase.where("'#{query}' #{like} '%' ||  phrase || '%'").order("LENGTH(phrase) ASC")
 		phrases = phrases.sort{|a, b| string_distance(query, a.phrase) <=> string_distance(query, b.phrase)}
-		phrase = phrases[0]	
+		phrase = phrases[0]
+
+		if phrase.nil?
+			flash.now[:warning] = "Invalid query"
+			return false
+		end
 
 		# Check if phrase isn't found
 		@intent = phrase.intent
@@ -23,7 +33,6 @@ class App::QueriesController < App::BaseController
 		end
 
 		@news = Stock.get_news stocks.map{|stock| stock.ticker}
-
 
 	end
 
