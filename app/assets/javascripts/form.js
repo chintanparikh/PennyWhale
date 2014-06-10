132bdfb5aaebac77f466043472eafcbcd5a0f735
Chintan Parikh <chintan@pennywhale.com>
2014-06-10 00:36:02 -0400

    Finish frontend for signup page

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -26,5 +26,19 @@ $(document).ready(function() {
 		$("#form-input-group").append("<p>You've signed up! Email <a href='mailto:hello@pennywhale.com'>hello@pennywhale.com</a> if you have any questions or features requests");
 	});
 
+	$('#card-form').card({
+	    // a selector or jQuery object for the container
+	    // where you want the card to appear
+	    container: '.card-wrapper', // *required*
+	    numberInput: '#card_number', // optional — default input[name="number"]
+	    expiryMonthInput: '#card_month', // optional — default input[name="expiry"]
+	    expiryYearInput: '#card_year',
+	    cvcInput: '#card_code', // optional — default input[name="cvc"]
+	    nameInput: '#card_name', // optional - defaults input[name="name"]
+
+	    width: 300, // optional — default 350px
+	    formatting: true // optional - default true
+	});
+
 
 });
