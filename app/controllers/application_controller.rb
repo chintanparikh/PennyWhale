ed588afb0dff4d20cea41625ed1ef2e862fce711
Chintan Parikh <chintan@pennywhale.com>
2014-06-10 14:15:41 -0400

    Live chat

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,6 @@ class ApplicationController < ActionController::Base
   protected 
   def configure_permitted_parameters
     #This allows the attributes to be accessible at sign up. I had to add email and password after adding token.
-    debugger
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :stripe_token, :email, :password, :coupon) }
   end
 end
