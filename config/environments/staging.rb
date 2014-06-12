b04b84649a92fbf6b99839cb0718eff6cfd43307
Chintan Parikh <chintan@pennywhale.com>
2014-06-12 16:17:37 -0400

    Change domain

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],  
-    :domain => 'heroku.com',
+    :domain => 'staging.pennywhale.com',
   }
   config.action_mailer.default_url_options = { :host => 'staging.pennywhale.com' }
 
