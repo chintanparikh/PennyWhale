c32887193d7c97ab263d50f9abd0846f3aa357d7
Chintan Parikh <chintan@pennywhale.com>
2014-06-12 16:29:41 -0400

    Change port back to string

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
-    :port => 25,
+    :port => '25',
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],  
