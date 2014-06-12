9087bd383d06d5c63e270fae6b447aac67eaf0ac
Chintan Parikh <chintan@pennywhale.com>
2014-06-12 18:07:06 -0400

    Switch tomandrill

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -82,17 +82,16 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => '25',
-    :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],  
-    :domain => 'staging.pennywhale.com',
+      :port =>           '587',
+      :address =>        'smtp.mandrillapp.com',
+      :user_name =>      ENV['MANDRILL_USERNAME'],
+      :password =>       ENV['MANDRILL_APIKEY'],
+      :domain =>         'heroku.com',
+      :authentication => :plain
   }
+  ActionMailer::Base.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'staging.pennywhale.com' }
-
 
 
 end
