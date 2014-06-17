11d829a1e091716c1a93922580481125ee965d78
Jay Deuskar <coffeejay@me.com>
2014-06-17 15:02:27 -0400

    highlighting for financials done

--- app/assets/javascripts/app/queries.js
+++ app/assets/javascripts/app/queries.js
@@ -26,7 +26,80 @@ $(document).ready(function()
 		preventBadQueries: false
 	});
 
+	//financials highlighting 
+
+	$('input').click(function(){
+
+		//check if its the first checkbox
+
+		var first_rows = []
+		$('tbody').each(function(index, elem) {
+				first_rows.push($(elem).children()[1]);
+		});
+
+		if (first_rows.indexOf($(this).parent().parent()[0]) > -1) {
+			console.log($(this).parent().parent().parent().find('input'));
+			var inputs = [];
+			
+			$(this).parent().parent().parent().find('input').each(function(){
+				inputs.push($(this));
+			});
+
+			inputs.shift();
+
+			$(inputs).each(function(){
+				$(this).click();
+			});	
+		}
+
+		// otherwise just handle that shit 
+
+		var row = $($(this).parent()).parent();
+
+		if ($(this).prop('checked')){
+			var elements = [];
+			$(row).children().each(function(){
+				elements.push($(this));
+			});
+
+			elements.shift();
+			elements.pop();
+			
+			$(elements).each(function(index){
+				if (index < elements.length-1){
+					var first_num = parseInt($(this).html().replace('$','').replace(',','').replace('.', ''))
+					var second_num = parseInt($(elements[index+1]).html().replace('$','').replace(',','').replace('.', ''))
+					if (second_num > first_num){
+						$(elements[index+1]).css('background-color', 'rgba(53, 207, 53, 0.39)');
+					}
+					else if (second_num < first_num){
+						$(elements[index+1]).css('background-color', 'rgba(246, 30, 30, 0.43)');	
+					}
+					else
+						null;
+				}
+				
+			});
+		}
+		else
+		{
+			$($(row).children()).each(function(){
+				$(this).css('background-color','#FFFFFF');
+			});
+
+		}
+		
+
+	});
+
+	
+
+
+
 
 	
 });
+
+
+
 
